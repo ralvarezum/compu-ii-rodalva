f15e4041e90c3b2219a7923c345d1345c4654a43
Rodrigo <rorototi@hotmail.com>
2023-05-02 13:04:19 -0300

    tp1 sin coms - rodri alva

---
+++ TP1/tp1.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+import sys
+
+def invertir_lineas(linea):
+    return linea.strip()[::-1]
+
+def main():
+    parser = argparse.ArgumentParser(description='Invierte cada línea de un archivo.')
+    parser.add_argument('archivo', help='Archivo a procesar.')
+    args = parser.parse_args()
+
+    try:
+        with open(args.archivo, 'r') as file:
+            lineas = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: el archivo {args.archivo} no se encontró.")
+        sys.exit(1)
+
+    pipes = []
+    for linea in lineas:
+        r, w = os.pipe()
+        pid = os.fork()
+        if pid == 0:
+            os.close(r)
+            w = os.fdopen(w, 'w')
+            w.write(invertir_lineas(linea))
+            w.close()
+            sys.exit(0)
+        else:
+            os.close(w)
+            pipes.append(r)
+
+    results = []
+    for pipe in pipes:
+        r = os.fdopen(pipe)
+        result = r.readline().strip()
+        results.append(result)
+        r.close()
+
+    for _ in lineas:
+        os.wait()
+
+    for result in results:
+        print(result, end='\n')
+
+if __name__ == '__main__':
+    main()
